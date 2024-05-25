a9b45f575f954fce46ce20c7eda5914dc499d52d
Diogo <diogokimura94@gmail.com>
2024-05-25 19:32:22 -0300

    adding maven publishing config

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -30,8 +31,36 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-}
 
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                groupId = "com.kimurashin"
+                artifactId = "sdk"
+                version = "0.1"
+
+                afterEvaluate {
+                    from(components["release"])
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                name = "simplesdk"
+                url = uri(layout.buildDirectory.dir("repo"))
+            }
+        }
+    }
+}
 dependencies {
 
     implementation(libs.androidx.core.ktx)
